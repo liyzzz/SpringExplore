ff85b6134da6d706e1c92fa16e3770457fc0020c
liyzzz <mail@liyueze.com>
2019-05-24 18:01:31 +0800

    add handle method

--- springExploreCode/src/main/java/com/liyueze/mvcFrameworkCode/v2/springmvn/servlet/HandlerAdapter.java
+++ springExploreCode/src/main/java/com/liyueze/mvcFrameworkCode/v2/springmvn/servlet/HandlerAdapter.java
@@ -1,7 +1,11 @@
 package com.liyueze.mvcFrameworkCode.v2.springmvn.servlet;
 
+import com.liyueze.mvcFrameworkCode.v2.annotation.RequestParamV2;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 public class HandlerAdapter {
     //那种adapter只支持那种Handler
@@ -9,6 +13,18 @@ public class HandlerAdapter {
 
     //具体执行HandleMapping中保存的方法
     ModelAndView handle(HttpServletRequest request, HttpServletResponse response, HandlerMapping handler) throws Exception{
+        Object controller=handler.getController();
+        Method method=handler.getMethod();
+        Annotation[][] parametersAnnotations=method.getParameterAnnotations();
+        for(Annotation[] annotations:parametersAnnotations){
+            for(Annotation annotation:annotations){
+                if(RequestParamV2.class==annotation.annotationType()){
+                    String paramName=((RequestParamV2)annotation).value();
+                    String  paramValue=request.getParameter(paramName);
+                    method.getParameterTypes();
+                }
+            }
+        }
         return null;
     }
 
